75ce88c800dfedcedffd2adcfa9049a85c1decc6
LeO <devgru0114@gmail.com>
2022-09-12 16:04:38 +0100

    Install and setup sentry

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
 import router from './router'
 import client from './client';
 import store from "./store/index";
@@ -6,8 +8,22 @@ import App from './App.vue'
 
 import './css/style.css'
 
-
 const app = createApp(App)
+
+Sentry.init({
+  app,
+  dsn: "https://953f58a4378248edb1130131172c697b@o1406871.ingest.sentry.io/6741154",
+  integrations: [
+    new BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      tracingOrigins: ["app.topaly.xyz", "local-app.topaly.xyz", /^\//],
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  logErrors: true, // log errors to console
+  environment: import.meta.env.VITE_NODE_ENV,
+});
+
 app.use(router)
 app.use(client) 
 app.use(store)
