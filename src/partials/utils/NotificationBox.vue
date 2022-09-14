432d15b633b684d145dbc6d25bc0fefbe5936279
LeO <devgru0114@gmail.com>
2022-09-14 16:12:19 +0100

    Add first real cypress tests

--- src/partials/utils/NotificationBox.vue
+++ src/partials/utils/NotificationBox.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="mt-5" v-show="message">
+  <div id="notification" class="mt-5" v-show="message">
     <div 
       class="px-3 py-2 rounded" 
       :class="isError ? 'bg-rose-100 text-rose-600' : 'bg-emerald-100 text-emerald-600'"
