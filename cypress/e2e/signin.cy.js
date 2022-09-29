8af32604a19d057a9bfec45a0b3e25f3b227a6c7
LeO <devgru0114@gmail.com>
2022-09-29 12:35:46 +0100

    disable cypress

--- cypress/e2e/signin.cy.js
+++ cypress/e2e/signin.cy.js
@@ -1,14 +1,14 @@
-describe('Signin User Not Registered', () => {
-  it('clicking "type" navigates to a new url', () => {
-    cy.visit('https://app.lasso.so/signin')
-    cy.get('input[id="email"]')
-      .type('fake@email.com')
-      .should('have.value', 'fake@email.com')
-    cy.get('input[id="password"]')
-      .type('supersecretpassword123%%')
-      .should('have.value', 'supersecretpassword123%%')
-    cy.contains('Sign In').click()
-    // cy.get('notification')
-    //   .should('contain', 'Please, enter valid credentials')
-  })
-})
+// describe('Signin User Not Registered', () => {
+//   it('clicking "type" navigates to a new url', () => {
+//     cy.visit('https://app.lasso.so/signin')
+//     cy.get('input[id="email"]')
+//       .type('fake@email.com')
+//       .should('have.value', 'fake@email.com')
+//     cy.get('input[id="password"]')
+//       .type('supersecretpassword123%%')
+//       .should('have.value', 'supersecretpassword123%%')
+//     cy.contains('Sign In').click()
+//     // cy.get('notification')
+//     //   .should('contain', 'Please, enter valid credentials')
+//   })
+// })
