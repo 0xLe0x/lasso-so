b4b8e4aa10948c15e7dbec4cc3ebe6e83480f392
LeO <devgru0114@gmail.com>
2022-10-31 19:34:45 +0000

    Add social icons (#11)

--- src/partials/dashboard/PlatformFilterCard.vue
+++ src/partials/dashboard/PlatformFilterCard.vue
@@ -8,9 +8,12 @@
         <div class="flex flex-col h-full">
           <!-- Card content -->
           <div class="grid grid-flow-rows grid-cols-12 gap-4 grow">
-            <ul class="col-span-12 md:col-span-4 lg:col-span-3 space-y-1 text-sm text-slate-800 mt-3 mb-4" v-for="platform in platforms" :key="platform">
-              <input @change="updateStore" :id="platform" :value="platform" type="checkbox" v-model="checkedPlatforms" class="ml-1 mr-3 w-4 h-4 text-indigo-500 bg-white rounded border-slate-300 focus:ring-indigo-300"/>
-              <label class="text-sm font-medium text-slate-800" :for="platform"><span>{{ platform }} </span></label>
+            <ul class="col-span-12 md:col-span-4 lg:col-span-3 space-y-1 text-sm text-slate-800 mt-3 mb-4" v-for="platform in platforms" :key="platform.name">
+              <div>
+                <input @change="updateStore" :id="platform.name" :value="platform.name" type="checkbox" v-model="checkedPlatforms" class="ml-1 mr-3 w-4 h-4 text-indigo-500 bg-white rounded border-slate-300 focus:ring-indigo-300"/>
+                <img :src="platform.image" class="w-6 h-6 inline ml-2 mr-2"/>
+                <label class="text-sm font-medium text-slate-800" :for="platform.name"><span>{{ platform.name }}</span></label>
+              </div>
             </ul>
           </div>
         </div>
@@ -20,6 +23,14 @@
 </template>
 
 <script>
+import TwitterImage from '../../images/socials/twitter.png';
+import InstagramImage from '../../images/socials/instagram.png';
+import YoutubeImage from '../../images/socials/youtube.png';
+import MediumImage from '../../images/socials/medium.png';
+import SubstackImage from '../../images/socials/substack.png';
+import TiktokImage from '../../images/socials/tiktok.png';
+import LinkedinImage from '../../images/socials/linkedin.png';
+
 
 export default {
   name: 'PlatformFilterCard',
@@ -27,7 +38,37 @@ export default {
   },
   data() {
     return {
-      platforms: ['Instagram', 'TikTok', 'YouTube', 'Twitter', 'LinkedIn', 'Medium', 'Substack'],
+      platforms: [
+        {
+            name: 'Instagram',
+            image: InstagramImage,
+        },
+        {
+            name: 'TikTok',
+            image: TiktokImage,
+        },
+        {
+            name: 'Youtube',
+            image: YoutubeImage,
+        },
+        {
+            name: 'Twitter',
+            image: TwitterImage,
+        },
+
+        {
+            name: 'LinkedIn',
+            image: LinkedinImage,
+        },
+        {
+            name: 'Medium',
+            image: MediumImage,
+        },
+        {
+            name: 'Substack',
+            image: SubstackImage,
+        }
+      ],
       checkedPlatforms: [],
     }
   },
