68c3ef5e8ea22e0c20a5b2cc4199a4ff8ac1181e
LeO <devgru0114@gmail.com>
2022-11-14 12:07:50 +0000

    add home banner (#13)

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -17,7 +17,8 @@
         <div class="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
           <div class="flex grow flex-col col-span-full bg-white rounded-sm">
             <div class="px-10 pb-4 pt-8">
-              <h1 class="text-2xl font-semibold text-slate-800 mb-2">Home</h1>
+              <h1 class="text-2xl font-semibold text-slate-800 mb-4">Home</h1>
+              <Banner heading="Hey 👋, let's get you setup with Lasso!" />
             </div>
           </div>
         </div>
@@ -30,10 +31,12 @@
 import { ref } from "vue";
 import Sidebar from "../partials/Sidebar.vue";
 import Header from "../partials/Header.vue";
+import Banner from "../partials/utils/Banner.vue";
 
 export default {
   name: "Home",
   components: {
+    Banner,
     Sidebar,
     Header,
   },
